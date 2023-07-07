b83d3d5d70b82101c0d65f4a9d50f99371b86dff
thienmpham <thienmpham@gmail.com>
2023-07-07 18:08:57 -0400

    WTF IS GOING ONNNNN!!!
    I did all this work just for the javascript code to not work for some reason. I spent hours trying to figure out why but seeing how I am just a fairly new to js, I dont understand shit. But on the sunny side, I fixed my issue with the icons not going to the right.

--- script.js
+++ script.js
@@ -1,15 +1,43 @@
-// Close the dropdown if the user clicks outside of it
-window.onclick = function(e) {
-    if(!e.target.matches('.dropbtn')) {
-        var myDropDown = document.getElementById("myDropdown");
-        if (myDropDown.classList.remove('show')) {
-            myDropDown.classList.remove('show');
-        }
-    }
-}
+class Dropdown {
 
-// When the user clicks on the button, 
-// toggle between hiding and showing the dropdown content 
-function myFunction(){
-    document.getElementById("myDropdown").classList.toggle('show');
-}
+    constructor() {
+      this.dropdowns = document.querySelectorAll('.dropdown .dropdown-menu');
+      if (this.dropdowns.length) {
+        this.initialize();
+      }
+    }
+  
+  
+    initialize() {
+      document.addEventListener('click', e => {
+        if (e.target.classList.contains('dropdown-action')) {
+          this.hideOtherDropdowns(e.target);
+          this.handleClick(e.target);
+        } else {
+          this.hideOtherDropdowns(null);
+        }
+      });
+    }
+  
+    handleClick(dropdownAction) {
+      this.dropdowns.forEach(dropdown => {
+        if (dropdown.parentElement.contains(dropdownAction)) {
+          dropdown.classList.add('active');
+        } else {
+          dropdown.classList.remove('active');
+        }
+      });
+    }
+  
+    hideOtherDropdowns(dropdownAction) {
+  
+      this.dropdowns.forEach(dropdown => {
+        if (!dropdown.parentElement.contains(dropdownAction)) {
+          dropdown.classList.remove('active');
+        }
+      });
+    }}
+  
+  
+  
+  document.addEventListener('DOMContentLoaded', () => new Dropdown());
