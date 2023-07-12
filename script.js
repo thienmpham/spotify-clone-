5cf13ab8f91ed77d63b062e28bba4da8f71879e0
thienmpham <thienmpham@gmail.com>
2023-07-12 18:19:24 -0400

    Script still not working... but I think im on the right track
    I have decided to create another script, the logic seems to be there but it seems like I am missing something. I feel that I am close... another day maybe.

--- script.js
+++ script.js
@@ -45,24 +45,49 @@ class Dropdown {
   document.addEventListener('DOMContentLoaded', () => new Dropdown());
 
 // Add new playlist when "Create new playlist" link is clicked 
-function createPlaylist () {
+// function createPlaylist () {
 
-  document.addEventListener( 'DOMContentLoaded', function() {
-    // Get the create Playlist and playlist container elements
-    var createNewPlaylistElement = document.getElementById('createNewPlaylist'); 
-    var playlistContainerElement = document.getElementById('playlistContainer'); 
+//   //document.addEventListener( 'DOMContentLoaded', function() {
+//     // Get the create Playlist and playlist container elements
+//     var createNewPlaylistElement = document.getElementById('createNewPlaylist'); 
+//     var playlistContainerElement = document.getElementById('playlistContainer'); 
     
-    createNewPlaylistElement.addEventListener('click', function(event) {
-      event.preventDefault();
+//     createNewPlaylistElement.addEventListener("click", function(event) {
+//       event.preventDefault();
       
-    // Get the reference to an already existing playlist element
-    var existingPlaylist = document.querySelector('.playlist-content');
-    
-    // Create a new playlist element as a copy of the existing element
-    var newPlaylist = existingPlaylist.cloneNode(true);
+//     // Get the reference to an already existing playlist element
+//     var existingPlaylist = document.querySelector('.playlist-content');
+//     //console.log(".playlist-content")
+//     // Create a new playlist element as a copy of the existing element
+//     var newPlaylist = existingPlaylist.cloneNode(true);
 
-    // Append the playlist element to the container 
-    playlistContainerElement.appendChild(newPlaylist);
-  });
-});
-}
+//     // Append the playlist element to the container 
+//     playlistContainerElement.appendChild(newPlaylist);
+//   });
+// //});
+// }
+
+function createPlaylist () {
+  // 1. Create div element 
+  let newPlaylistDiv = document.createElement('div'); 
+  // The new div will have elements from class: playlist-content
+  newPlaylistDiv.classList.add('playlist-content');
+
+  // Adds the newPlaylistDiv to the playlist container
+  playlistContainerDiv.appendChild(newPlaylistDiv);
+
+
+
+
+  }
+  //Get the container of the playlist 
+  let playlistContainerDiv = document.querySelector('.user-playlist'); 
+  
+  // // 2. Adds an event listener to the link button "Create new playlist"
+  //  document.getElementById('createNewPlaylist').addEventListener('click', function(){
+  //    alert("Div was clicked!");
+  //  }); 
+
+    //  Adds an event listener to the link button "Create new playlist"
+    const createNewPlaylist = document.getElementById('createNewPlaylist');
+    createNewPlaylist.addEventListener('click', createPlaylist);
