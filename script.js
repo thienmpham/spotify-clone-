0d7fd394f7c4fab01dfa46e04361e54be620cf5d
thienmpham <thienmpham@gmail.com>
2023-07-19 20:56:52 -0400

    Found 1 solution
    I found 1 solution for my problem of the createPlaylist function appending a new playlist before the link is clicked. Tomorrow I will have find a better way or just organize my code better. Wish me luck fellow crazies!

--- script.js
+++ script.js
@@ -2,88 +2,99 @@
 // For the dropdown menu
 class Dropdown {
 
-    constructor() {
-      this.dropdowns = document.querySelectorAll('.dropdown .dropdown-menu');
-      if (this.dropdowns.length) {
-        this.initialize();
+  constructor() {
+    this.dropdowns = document.querySelectorAll('.dropdown .dropdown-menu');
+    if (this.dropdowns.length) {
+      this.initialize();
+    }
+  }
+
+
+  initialize() {
+    document.addEventListener('click', e => {
+      //if (e.target.classList.contains('dropdown-action')) {
+        this.hideOtherDropdowns(e.target);
+        this.handleClick(e.target);
+      /*} else {
+        this.hideOtherDropdowns(null);
+      }*/
+    });
+  }
+
+  handleClick(dropdownAction) {
+    this.dropdowns.forEach(dropdown => {
+      if (dropdown.parentElement.contains(dropdownAction)) {
+        dropdown.classList.add('active');
+      } else {
+        dropdown.classList.remove('active');
       }
-    }
-  
-  
-    initialize() {
-      document.addEventListener('click', e => {
-        //if (e.target.classList.contains('dropdown-action')) {
-          this.hideOtherDropdowns(e.target);
-          this.handleClick(e.target);
-        /*} else {
-          this.hideOtherDropdowns(null);
-        }*/
-      });
-    }
-  
-    handleClick(dropdownAction) {
-      this.dropdowns.forEach(dropdown => {
-        if (dropdown.parentElement.contains(dropdownAction)) {
-          dropdown.classList.add('active');
-        } else {
-          dropdown.classList.remove('active');
-        }
-      });
-    }
-  
-    hideOtherDropdowns(dropdownAction) {
-  
-      this.dropdowns.forEach(dropdown => {
-        if (!dropdown.parentElement.contains(dropdownAction)) {
-          dropdown.classList.remove('active');
-        }
-      });
-    }}
-  
-  
-  
-  document.addEventListener('DOMContentLoaded', () => new Dropdown());
-  
-  document.addEventListener('DOMContentLoaded', () => new createPlaylist());
+    });
+  }
+
+  hideOtherDropdowns(dropdownAction) {
+
+    this.dropdowns.forEach(dropdown => {
+      if (!dropdown.parentElement.contains(dropdownAction)) {
+        dropdown.classList.remove('active');
+      }
+    });
+  }}
+
+
+
+document.addEventListener('DOMContentLoaded', () => new Dropdown());
+
+
 
 function createPlaylist () {
-  // 1. Define and Create div element 
-  let div = document.createElement('div'); 
-  
-  // 2. Define and Get the HTML collection  of playlist content
-  let playlistContainerDiv = document.getElementById('new-playlist-content');
-  // Clone the playistContainerDiv so that it does not target an already existing playlist content
-  const clonedPlaylistContainerDiv = playlistContainerDiv.cloneNode(true);
-  // Assign unique id to cloned div 
-  clonedPlaylistContainerDiv.id = 'cloned-playlist-content';
 
-  console.log(clonedPlaylistContainerDiv);
+// 1. Define and Create div element 
+let div = document.createElement('div'); 
 
-  // 3.playListContainerDiv element will be added to the empty div 
-  div.append(clonedPlaylistContainerDiv);
-  console.log(div);
-  
+// 2. Define and Get the HTML collection  of playlist content
+let playlistContainerDiv = document.getElementById('new-playlist-content');
+// Clone the playistContainerDiv so that it does not target an already existing playlist content
+const clonedPlaylistContainerDiv = playlistContainerDiv.cloneNode(true);
+// Assign unique id to cloned div 
+clonedPlaylistContainerDiv.id = 'cloned-playlist-content';
 
-  //Add new div to the class .user-playlist
-  let userPlaylist = document.getElementById('user-playlist-container');
-  console.log(userPlaylist);
-  userPlaylist.appendChild(div);
-  
-  
+console.log(clonedPlaylistContainerDiv);
 
+// 3.playListContainerDiv element will be added to the empty div 
+div.append(clonedPlaylistContainerDiv);
+console.log(div);
+
+
+//Add new div to the class .user-playlist
+let userPlaylist = document.getElementById('user-playlist-container');
+console.log(userPlaylist);
+userPlaylist.appendChild(div);
 
 
 
-  };
-  
- // 4. Add an event listener to the "Create new playlist" dropdown option 
- document.addEventListener('DOMContentLoaded',function handlePlaylistClick()  {
- const createNewPlaylistOption = document.getElementById('create-new-playlist-option');
- console.log(createNewPlaylistOption);
 
+
+
+};
+
+// 4. Add an event listener to the "Create new playlist" dropdown option 
+document.addEventListener('DOMContentLoaded',function handlePlaylistClick()  {
+const createNewPlaylistOption = document.getElementById('create-new-playlist-option');
+console.log(createNewPlaylistOption);
+
+
+createNewPlaylistOption.addEventListener('click', createPlaylist);
+
+});
  
- createNewPlaylistOption.addEventListener('click', createPlaylist);
 
- });
+  
+
+
+
+
+  
+  
+
    
   
