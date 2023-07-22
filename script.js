5cef601438a3cf96a805b717b175e144320539e2
thienmpham <thienmpham@gmail.com>
2023-07-22 12:40:14 -0400

    Figured out the problem of 2 created playlists
    I figured out how why 2 created playlists were created and why one div contained all of my newly created playlists. Now onto local storage and updating the playlist # automatically!!! HOORAHHHH!! FOR SPARTA!!!!

--- script.js
+++ script.js
@@ -44,51 +44,45 @@ class Dropdown {
 
 
   document.addEventListener('DOMContentLoaded', () => new Dropdown());
+
   
-  document.addEventListener('DOMContentLoaded', () => new createPlaylist());
-// 1. Define and Create div element 
-let div = document.createElement('div'); 
+
+
+      
 
 // 2. Define and Get the HTML collection  of playlist content
-const playlistContainerDiv = document.getElementById('new-playlist-content');
+      const playlistContainerDiv = document.getElementById('new-playlist-content');
 
 let userPlaylist = document.getElementById('user-playlist-container');
-console.log(userPlaylist);
-
-  
-function createPlaylist () {
-    // Clone the playistContainerDiv so that it does not target an already existing playlist content
-    const clonedPlaylistContainerDiv = playlistContainerDiv.cloneNode(true);
-    // Assign unique id to cloned div 
-    clonedPlaylistContainerDiv.id = 'cloned-playlist-content';
-
-    console.log(clonedPlaylistContainerDiv);
-
-    // 3.playListContainerDiv element will be added to the empty div 
-    div.append(clonedPlaylistContainerDiv);
-  
-    //Add new div to the class .user-playlist
-
-    
-
-
-};
+      console.log(userPlaylist);
 
 // 4. Add an event listener to the "Create new playlist" dropdown option 
-document.addEventListener('DOMContentLoaded',function handlePlaylistClick()  {
-const createNewPlaylistOption = document.getElementById('create-new-playlist-option');
-console.log(createNewPlaylistOption);
+      document.addEventListener('DOMContentLoaded',function handlePlaylistClick()  {
+      const createNewPlaylistOption = document.getElementById('create-new-playlist-option');
+      console.log(createNewPlaylistOption);
 
 
-//createNewPlaylistOption.addEventListener('click', createPlaylist);
-  createNewPlaylistOption.addEventListener('click', function() {
-    userPlaylist.prepend(div);
-    createPlaylist();
+createNewPlaylistOption.addEventListener('click', function() {
+      // 1. Define and Create div element 
+      let div = document.createElement('div'); 
+       // Clone the playistContainerDiv so that it does not target an already existing playlist content
+       const clonedPlaylistContainerDiv = playlistContainerDiv.cloneNode(true);
+       // Assign unique id to cloned div 
+       clonedPlaylistContainerDiv.id = 'cloned-playlist-content';
+       div.id = 'cloned-playlist-div';
+       userPlaylist.prepend(div);
+   
+       // 3.playListContainerDiv element will be added to the empty div 
+       div.append(clonedPlaylistContainerDiv);
+     
+    console.log('hi');
   });
 });
  
 
-
+// Get amount of elements in div 
+numberOfPlaylist = userPlaylist.children.length;
+console.log(numberOfPlaylist);
 
 
 
