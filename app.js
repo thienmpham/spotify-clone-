e150128f8312a1740cc703f004f9465526f5bcd5
thienmpham <thienmpham@gmail.com>
2023-08-10 14:08:35 -0400

    Made another js file called app.js for the api
    I am still working on getting the access token. What I am confused about is using the query parameters and how to redirect that to the Spotify authorization page. I will figure this out, hopefully sooner rather than later...

---
+++ app.js
@@ -0,0 +1,12 @@
+
+//
+// Authorization for Spotify 
+//
+function requestAuthorization(){
+    const CLIENT_ID = "d119d36427944ecd8e12365f96cea46e"; 
+    const REDIRECT_URI = "http://localhost:3000";
+    const AUTH_ENDPOINT  = "https://accounts.spotify.com/authorize";
+    const RESPONSE_TYPE = "token";
+    
+  
+  }
