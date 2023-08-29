88e12be51e828217be40d0dd7ed9cd1d4868192a
thienmpham <thienmpham@gmail.com>
2023-08-28 21:21:15 -0400

    Still trying to solve how to import js variables
    I am still trying to figure out how to export my code variable inside my app.js into my server.js. The server.js is a node.js file which I am going to use to request access tokens for the Spotify API!

--- server/app.js
+++ server/app.js
@@ -28,71 +28,71 @@ function onPageLoad(){
     }
 }
 
-export function handleRedirect(){
-    let code = getCode();  
-    fetchAccessToken( code );
-    //window.history.pushState("", "", redirect_uri); //remove param from url
+// export function handleRedirect(){
+//     let code = getCode();  
+//     fetchAccessToken( code );
+//     //window.history.pushState("", "", redirect_uri); //remove param from url
     
-}
+// }
 
-function fetchAccessToken( code ){
-  let body = "grant_type=authorization_code";
-  body += "&code=" + code;
-  body += "&redirect_uri=" + encodeURI(redirect_uri);
-  body += "&client_id=" + client_id;
-  body += "&client_secret=" + client_secret;
-  callAuthorizationApi(body);
-  console.log(body);
-}
+// function fetchAccessToken( code ){
+//   let body = "grant_type=authorization_code";
+//   body += "&code=" + code;
+//   body += "&redirect_uri=" + encodeURI(redirect_uri);
+//   body += "&client_id=" + client_id;
+//   body += "&client_secret=" + client_secret;
+//   callAuthorizationApi(body);
+  
+// }
 
-function callAuthorizationApi(body){
-    let xhr = new XMLHttpRequest(); 
-    xhr.open("POST", TOKEN, true); 
-    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-    xhr.setRequestHeader('Authorization', 'Basic' + btoa(client_id + ":" + client_secret));
-    xhr.send(body);
-    xhr.onload = handleAuthorizationResponse;
-    console.log(xhr);
-}
+// function callAuthorizationApi(body){
+//     let xhr = new XMLHttpRequest(); 
+//     xhr.open("POST", TOKEN, true); 
+//     xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+//     xhr.setRequestHeader('Authorization', 'Basic' + btoa(client_id + ":" + client_secret));
+//     xhr.send(body);
+//     xhr.onload = handleAuthorizationResponse;
+    
+// }
 
 
-function handleAuthorizationResponse(){
-  if (this.status == 200){
-    var data = JSON.parse(this.responseText);
-    console.log(data);
-    var data = JSON.parse(this.responseText);
-    if ( data.access_token != undefined ){
-      access_token = data.access_token;
-      localStorage.setItem("access_token", access_token);
-    }
-    if ( data.refresh_token != undefined ){
-      refresh_token = data.refresh_token;
-      localStorage.setItem("refresh_token", refresh_token);
-    }
-    onPageLoad();
-}
-  else {
-    console.log(this.responseText);
-    alert(this.responseText);
-  }
-}
+// function handleAuthorizationResponse(){
+//   if (this.status == 200){
+//     var data = JSON.parse(this.responseText);
+//     console.log(data);
+//     var data = JSON.parse(this.responseText);
+//     if ( data.access_token != undefined ){
+//       access_token = data.access_token;
+//       localStorage.setItem("access_token", access_token);
+//     }
+//     if ( data.refresh_token != undefined ){
+//       refresh_token = data.refresh_token;
+//       localStorage.setItem("refresh_token", refresh_token);
+//     }
+//     onPageLoad();
+// }
+//   else {
+//     console.log(this.responseText);
+//     alert(this.responseText);
+//   }
+// }
 
 
 // Parses through the url 
 // and gets the value
 // of code 
-function getCode(){
-  let code = null;
-  const queryString = window.location.search;
-  if ( queryString.length > 0 ){
-    const urlParams = new URLSearchParams(queryString); // Turns URL into URLSearchParams object to through parse easier
-    code = urlParams.get("code")
-    console.log(code);
-    window.localStorage.setItem("code", code);
-  }
-  return code;
+// export function getCode(){
+//   let code = null;
+//   const queryString = window.location.search;
+//   if ( queryString.length > 0 ){
+//     const urlParams = new URLSearchParams(queryString); // Turns URL into URLSearchParams object to through parse easier
+//     code = urlParams.get("code");
+//     console.log(code);
+//     window.localStorage.setItem("code", code);
+//   }
+//   return code;
   
-}
+// }
 
 
 // function initalizeGlobals(callback) {
@@ -115,6 +115,7 @@ function getCode(){
     url += "&redirect_uri=" + encodeURI(redirect_uri);
     url += "&show_dialog=false";
     url += "&scope=user-read-private ugc-image-upload user-read-playback-state user-modify-playback-state user-read-currently-playing streaming playlist-modify-private playlist-modify-public user-follow-modify user-follow-read user-read-playback-position user-top-read user-read-recently-played user-library-modify user-library-read user-read-email";
-    } 
+    window.location.href = url;
+  } 
   
 
--- server/server.js
+++ server/server.js
@@ -1,4 +1,54 @@
 // server = require("../client/app.js")
 
-import { handleRedirect } from './app.js'
- 
+import { getCode } from './app.js'
+
+console.log(getCode);
+
+function handleRedirect(){
+    let code = getCode();  
+    fetchAccessToken( code );
+    //window.history.pushState("", "", redirect_uri); //remove param from url
+    console.log(code);
+}
+
+function fetchAccessToken( code ){
+  let body = "grant_type=authorization_code";
+  body += "&code=" + code;
+  body += "&redirect_uri=" + encodeURI(redirect_uri);
+  body += "&client_id=" + client_id;
+  body += "&client_secret=" + client_secret;
+  callAuthorizationApi(body);
+  
+}
+
+function callAuthorizationApi(body){
+    let xhr = new XMLHttpRequest(); 
+    xhr.open("POST", TOKEN, true); 
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    xhr.setRequestHeader('Authorization', 'Basic' + btoa(client_id + ":" + client_secret));
+    xhr.send(body);
+    xhr.onload = handleAuthorizationResponse;
+    
+}
+
+
+function handleAuthorizationResponse(){
+  if (this.status == 200){
+    var data = JSON.parse(this.responseText);
+    console.log(data);
+    var data = JSON.parse(this.responseText);
+    if ( data.access_token != undefined ){
+      access_token = data.access_token;
+      localStorage.setItem("access_token", access_token);
+    }
+    if ( data.refresh_token != undefined ){
+      refresh_token = data.refresh_token;
+      localStorage.setItem("refresh_token", refresh_token);
+    }
+    onPageLoad();
+}
+  else {
+    console.log(this.responseText);
+    alert(this.responseText);
+  }
+}
