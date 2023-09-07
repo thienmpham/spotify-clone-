3de8934567d16cdb38f6c22172ec3667e984c7e1
thienmpham <thienmpham@gmail.com>
2023-09-06 20:14:39 -0400

    Soooooo I went back to plain vanilla JS
    I decided to go back to vanilla JS rather than using a library so that I can learn more in depth of JS as a whole.
    
    Right now I am stuck on the part of getting the access tokens. For some reason, the API call is not working so I will let the tomorrow me figure that out :pig:

--- client/app.js
+++ client/app.js
@@ -1,127 +1,118 @@
-
-
-
-
-var redirect_uri = "http://127.0.0.1:5500/src/";
-var auth_endpoint  = "https://accounts.spotify.com/authorize";
-var TOKEN = "https://accounts.spotify.com/api/token";
+var redirect_uri = "http://127.0.0.1:5500/src/index.html";
+const auth_endpoint  = "https://accounts.spotify.com/authorize";
+const TOKEN = "https://accounts.spotify.com/api/token";
 var access_token = null;
 var refresh_token = null;
 
-
-var client_id = "930fa5bb5cfb41cd93ee92a8307286a8";
-// var client_secret = process.env.client_secret_key;
-
+var client_id = "";
+var client_secret = "";
 
 
 
-//
-// Authorization for Spotify 
-//
+function onPageLoad(){
+    client_id = localStorage.getItem("client_id");
+    client_secret = localStorage.getItem("client_secret");
+    if( window.location.length > 0){
+      handleRedirect();
+    }
+}
 
-
-
-// export function handleRedirect(){
-//     let code = getCode();  
-//     fetchAccessToken( code );
-//     //window.history.pushState("", "", redirect_uri); //remove param from url
+//Authorization for Spotify 
+function handleRedirect(){
+    let code = getCode();  
+    fetchAccessToken( code );
+    window.history.pushState("", "", redirect_uri); //remove param from url
     
-// }
+}
 
-// export function getCode(){
-//   let code = null;
-//   const queryString = window.location.search;
-//   if ( queryString.length > 0 ){
-//       const urlParams = new URLSearchParams(queryString);
-//       code = urlParams.get('code')
-//   }
-//   return code;
-// }
+function getCode(){
+  let code = null;
+  const queryString = window.location.search;
+  if ( queryString.length > 0 ){
+      const urlParams = new URLSearchParams(queryString);
+      code = urlParams.get('code')
+  }
+  return code;
+}
 
 
-// function fetchAccessToken( code ){
-//   let body = "grant_type=authorization_code";
-//   body += "&code=" + code;
-//   body += "&redirect_uri=" + encodeURI(redirect_uri);
-//   body += "&client_id=" + client_id;
-//   body += "&client_secret=" + client_secret;
-//   callAuthorizationApi(body);
+function fetchAccessToken( code ){
+  let body = "grant_type=authorization_code";
+  body += "&code=" + code;
+  body += "&redirect_uri=" + encodeURI(redirect_uri);
+  body += "&client_id=" + client_id;
+  body += "&client_secret=" + client_secret;
+  callAuthorizationApi(body);
   
-// }
+}
 
-// function callAuthorizationApi(body){
-//     let xhr = new XMLHttpRequest(); 
-//     xhr.open("POST", TOKEN, true); 
-//     xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-//     xhr.setRequestHeader('Authorization', 'Basic' + btoa(client_id + ":" + client_secret));
-//     xhr.send(body);
-//     xhr.onload = handleAuthorizationResponse;
+function callAuthorizationApi(body){
+    let xhr = new XMLHttpRequest(); 
+    xhr.open("POST", TOKEN, true); 
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    xhr.setRequestHeader('Authorization', 'Basic' + btoa(client_id + ":" + client_secret));
+    xhr.send(body);
+    xhr.onload = handleAuthorizationResponse;
     
-// }
+}
 
 
-// function handleAuthorizationResponse(){
-//   if (this.status == 200){
-//     var data = JSON.parse(this.responseText);
-//     console.log(data);
-//     var data = JSON.parse(this.responseText);
-//     if ( data.access_token != undefined ){
-//       access_token = data.access_token;
-//       localStorage.setItem("access_token", access_token);
-//     }
-//     if ( data.refresh_token != undefined ){
-//       refresh_token = data.refresh_token;
-//       localStorage.setItem("refresh_token", refresh_token);
-//     }
-//     onPageLoad();
-// }
-//   else {
-//     console.log(this.responseText);
-//     alert(this.responseText);
-//   }
-// }
+function handleAuthorizationResponse(){
+  if (this.status == 200){
+    var data = JSON.parse(this.responseText);
+    console.log(data);
+    var data = JSON.parse(this.responseText);
+    if ( data.access_token != undefined ){
+      access_token = data.access_token;
+      localStorage.setItem("access_token", access_token);
+    }
+    if ( data.refresh_token != undefined ){
+      refresh_token = data.refresh_token;
+      localStorage.setItem("refresh_token", refresh_token);
+    }
+    onPageLoad();
+}
+  else {
+    console.log(this.responseText);
+    alert(this.responseText);
+  }
+}
 
 
-// Parses through the url 
-// and gets the value
-// of code 
-// export function getCode(){
-//   let code = null;
-//   const queryString = window.location.search;
-//   if ( queryString.length > 0 ){
-//     const urlParams = new URLSearchParams(queryString); // Turns URL into URLSearchParams object to through parse easier
-//     code = urlParams.get("code");
-//     console.log(code);
-//     window.localStorage.setItem("code", code);
-//   }
-//   return code;
+//Parses through the url 
+//and gets the value
+//of code 
+function getCode(){
+  let code = null;
+  const queryString = window.location.search;
+  if ( queryString.length > 0 ){
+    const urlParams = new URLSearchParams(queryString); // Turns URL into URLSearchParams object to through parse easier
+    code = urlParams.get("code");
+    console.log(code);
+    window.localStorage.setItem("code", code);
+  }
   
-// }
+  
+}
 
-
-// function initalizeGlobals(callback) {
-//   client_id = process.env.client_id_key; 
-//   client_secret = process.env.client_secret_key;
-
-//   callback();
-
-// }
-
-
-// Creates a unique link to
-// access the Spotify Authorization page 
-// using the Query parameters in the documentation
+//Creates a unique link to
+//access the Spotify Authorization page 
+//using the Query parameters in the documentation
 
   function requestAuthorization(){   
+    client_id = document.getElementById("clientId").value;
+    client_secret = document.getElementById("clientSecret").value;
+    localStorage.setItem("client_id", client_id);
+    localStorage.setItem("client_secret", client_secret);
+
     let url = auth_endpoint;
     url += "?client_id=" + client_id;
     url += "&response_type=code";
     url += "&redirect_uri=" + encodeURI(redirect_uri);
-    url += "&show_dialog=false";
+    url += "&show_dialog=true";
     url += "&scope=user-read-private ugc-image-upload user-read-playback-state user-modify-playback-state user-read-currently-playing streaming playlist-modify-private playlist-modify-public user-follow-modify user-follow-read user-read-playback-position user-top-read user-read-recently-played user-library-modify user-library-read user-read-email";
-    window.location.href = url;
+    window.location.href = url; //redirects the url
   } 
-  document.getElementById('sign-in-button').addEventListener('click', requestAuthorization);
 
  
 
